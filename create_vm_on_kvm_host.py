25e9812117968f5056def29d9aa506d1a58c13ce
Umamaheshwar <urao@juniper.net>
2019-01-17 09:55:27 -0800

    Minor fixes

--- create_vm_on_kvm_host.py
+++ create_vm_on_kvm_host.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # Tested on Centos 7.4, works without changes for creating RHEL VMs
-# Bring you run this script, install KVM packages using script
+# Before you run this script, install KVM packages using script
 # https://github.com/urao/docker-k8s-project/blob/master/install_kvm.sh
+# Example
+# python create_vm_on_kvm_host.py -i rhel-server-7.5-x86_64-kvm.qcow2 -s 50G -n pp02 -p contrail123 -c 4 -r 32764 create
+# python create_vm_on_kvm_host.py -n pp01 delete
 
 import sys
 import subprocess
 import argparse
 import shlex
 import os
+import time
 
 
 class BuildVM(object):
@@ -20,20 +24,24 @@ class BuildVM(object):
         """
         Create a new VM
         """
-        f=open("create.log", mode='w')
+        log_file = 'create_%s.log'%(self.opt.vm_name)
+        print 'Creating VM, check log file %s for details'%(log_file)
+        f=open(log_file, mode='w')
         vm_name = str(self.opt.vm_name)+".qcow2"
 
-        command = 'export LIBGUESTFS_BACKEND=direct'
-        execute(command, f, ignore_errors=False)
+        os.environ["LIBGUESTFS_BACKEND"] = 'direct'
 
+        print 'Running qemu-img..'
         command = 'qemu-img create -f qcow2 -o preallocation=metadata'
         command += " " + vm_name + " "+ str(self.opt.vm_size)
         execute(command, f, ignore_errors=False)
 
+        print 'Running virt-resize..'
         command = 'virt-resize --expand /dev/sda1'
         command += " " + str(self.opt.qcow2_file)+ " "+ vm_name
         execute(command, f, ignore_errors=False)
 
+        print 'Running virt-customize..'
         command = 'virt-customize -a'
         command += " " + vm_name + " " + '--hostname' + " "+str(self.opt.vm_name)
         command += " " + '--timezone America/Los_Angeles --run-command \'xfs_growfs /\' --root-password'
@@ -47,6 +55,7 @@ class BuildVM(object):
         command += " " + '/var/lib/libvirt/images/'
         execute(command, f, ignore_errors=False)
 
+        print 'Running virt-install..'
         command = 'virt-install'
         command += " " + '--name' + " "+str(self.opt.vm_name)
         command += " " + '--ram' + " "+str(self.opt.vm_mem)
@@ -58,6 +67,8 @@ class BuildVM(object):
         command += " " + '--console pty,target_type=virtio'
         execute(command, f, ignore_errors=False)
 
+        print 'Sleeping for 60 seconds for VM to boot up...'
+        time.sleep(60)
         command = 'virsh domifaddr '+str(self.opt.vm_name)
         res = execute(command, f, ignore_errors=False)
         print 'IP address of the VM: '
@@ -68,7 +79,10 @@ class BuildVM(object):
         """
         Delete a VM
         """
-        f=open("delete.log", mode='w')
+        log_file = 'delete_%s.log'%(self.opt.vm_name)
+        print 'Deleting VM, check log file %s for details'%(log_file)
+        f=open(log_file, mode='w')
+        vm_name = str(self.opt.vm_name)+".qcow2"
         command = 'virsh destroy '+str(self.opt.vm_name)
         execute(command, f, ignore_errors=False)
         command = 'virsh undefine '+str(self.opt.vm_name)
@@ -78,7 +92,6 @@ class BuildVM(object):
         f.close()
 
 
-
 # Utility functions
 def cmd_exists(cmd):
     return subprocess.call("type "+ cmd, shell=True, 
@@ -86,6 +99,7 @@ def cmd_exists(cmd):
 
 def execute(cmd, output, ignore_errors=False):
 
+    print cmd
     pipe = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE,
                             stderr=subprocess.STDOUT, close_fds=True)
     data = ""
